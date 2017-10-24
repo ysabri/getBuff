77d1c905f492707f9223f197b85d67e943c42acb
Yazeed Sabri <ysabri@wisc.edu>
2017-10-23 20:52:50 -0500

    added the files with my changes

---
+++ BufMgr/src/buffer.cpp
@@ -0,0 +1,135 @@
+/**
+ * @author See Contributors.txt for code contributors and overview of BadgerDB.
+ *
+ * @section LICENSE
+ * Copyright (c) 2012 Database Group, Computer Sciences Department, University of Wisconsin-Madison.
+ */
+
+#include <memory>
+#include <iostream>
+#include "buffer.h"
+#include "exceptions/buffer_exceeded_exception.h"
+#include "exceptions/page_not_pinned_exception.h"
+#include "exceptions/page_pinned_exception.h"
+#include "exceptions/bad_buffer_exception.h"
+#include "exceptions/hash_not_found_exception.h"
+
+namespace badgerdb { 
+
+BufMgr::BufMgr(std::uint32_t bufs)
+	: numBufs(bufs) {
+	bufDescTable = new BufDesc[bufs];
+
+  for (FrameId i = 0; i < bufs; i++) 
+  {
+  	bufDescTable[i].frameNo = i;
+  	bufDescTable[i].valid = false;
+  }
+
+  bufPool = new Page[bufs];
+
+	int htsize = ((((int) (bufs * 1.2))*2)/2)+1;
+  hashTable = new BufHashTbl (htsize);  // allocate the buffer hash table
+
+  clockHand = bufs - 1;
+}
+
+
+BufMgr::~BufMgr() {
+  //flush pages that are dirty
+  for(FrameId i = 0; i < sizeof(bufDescTable); i++) {
+    if(bufDescTable[i].dirty==true){
+      flushFile(bufDescTable[i].file);
+    }
+  }
+  //dealocate bufDesc Array
+  delete[] bufDescTable;
+  //dealocate bufPool Array
+  delete[] bufPool;
+  //call this dealocation method since bufHashTbl class has none static variables
+  delete hashTable;
+}
+
+void BufMgr::advanceClock()
+{
+  clockHand++;
+  //migth be numbufs instead of sizeof(bufPool)
+  clockHand = clockHand % numBufs;
+  // FrameId curr = clockHand;
+  // if(curr++ >= sizeof(bufPool)){
+  //   clockHand = 0;
+  // } else {
+  //   clockHand++;
+  // }
+}
+
+void BufMgr::allocBuf(FrameId & frame) 
+{
+
+}
+
+	
+void BufMgr::readPage(File* file, const PageId pageNo, Page*& page)
+{
+
+}
+
+
+void BufMgr::unPinPage(File* file, const PageId pageNo, const bool dirty) 
+{
+}
+
+void BufMgr::flushFile(const File* file) 
+{
+  for(FrameId i=0; i<numBufs; i++) {
+    if(bufDescTable[i].file == file) {
+      if(bufDescTable[i].valid) {
+        if(bufDescTable[i].pinCnt==0){
+          if(bufDescTable[i].dirty){
+              //wait for piazza
+              //file->writePage(bufPool[i]);
+              bufDescTable[i].valid = false;
+          }
+        } else {
+            throw PagePinnedException("",bufDescTable[i].pageNo, 
+              bufDescTable[i].frameNo);
+        } 
+        hashTable->remove(file, bufDescTable[i].pageNo);
+        bufDescTable->Clear();
+        //bufDescTable[i] = *(new BufDesc());
+      } else {
+        throw BadBufferException(bufDescTable[i].frameNo,
+         bufDescTable[i].dirty, bufDescTable[i].valid, bufDescTable[i].refbit);
+      }
+    }
+  }
+}
+
+void BufMgr::allocPage(File* file, PageId &pageNo, Page*& page) 
+{
+}
+
+void BufMgr::disposePage(File* file, const PageId PageNo)
+{
+    
+}
+
+void BufMgr::printSelf(void) 
+{
+  BufDesc* tmpbuf;
+	int validFrames = 0;
+  
+  for (std::uint32_t i = 0; i < numBufs; i++)
+	{
+  	tmpbuf = &(bufDescTable[i]);
+		std::cout << "FrameNo:" << i << " ";
+		tmpbuf->Print();
+
+  	if (tmpbuf->valid == true)
+    	validFrames++;
+  }
+
+	std::cout << "Total Number of Valid Frames:" << validFrames << "\n";
+}
+
+}
